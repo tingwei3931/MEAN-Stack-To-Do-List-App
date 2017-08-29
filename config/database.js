ea48782f1a3cfc2236ff9e75369178d839c09875
Lim Ting Wei <tingwei3931@gmail.com>
2017-08-29 23:47:59 +0800

    linked db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb://tingwei3931:tingwei3931@@ds161503.mlab.com:61503/meanauthapptw',
     secret: 'yoursecret'
 }
